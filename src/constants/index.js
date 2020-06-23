be9ba9a0e49245ca1c23446bc5a5220ce33116da
Tarcisio Cardoso <tarcardoso@gmail.com>
2020-06-23 12:22:14 -0300

    Ajuste documento.origin para documento.location.origin

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,10 @@ export const ACCESS_TOKEN = 'accessToken';
 
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
 
-export const OAUTH2_REDIRECT_URI = document.origin+'/oauth2/redirect';
+export const OAUTH2_REDIRECT_URI = document.location.origin+'/oauth2/redirect';
+
+
+// export const OAUTH2_REDIRECT_URI = 'https://'+document.location.hostname+(document.location.port.length>0?':'+document.location.port:'')+'/oauth2/redirect';
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/api/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/api/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
